c8bd85a5ad77fd8bbb24d7a68fc19fa46cdd4811
Anirban Pal <anirbanpal1997@gmail.com>
2021-03-09 17:44:07 +0530

    styling and password validation added

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+
 function validateUname(){
     const uname = document.getElementById('Uname').value
     console.log(uname.length)
@@ -10,10 +11,54 @@ function validateUname(){
 function validateEmail(){
     const email = document.getElementById("usermail").value
     const mailformat =/^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/
-    // console.log(mailformat.test(email))
     
     if(!email.match(mailformat))
     document.getElementById("email_error").style.display='block'
     else
     document.getElementById("email_error").style.display='none'
 }
+
+function checkpassword1() {
+    if(password1.value==password2.value){
+        password2_error.style.display = 'none'
+    }
+    if (password1.value.length==0){
+        password1_error.style.display = 'block'
+        password1_error.innerHTML = "password can not be empty"
+    }
+    else{
+        password1_error.style.display = 'none'
+    }
+}
+
+function checkpassword2(){
+    if (password2.value.length==0){
+        password2_error.style.display = 'block'
+        password2_error.innerHTML = "password can not be empty"
+    }
+    else if(password1.value!=password2.value){
+        password2_error.style.display = 'block'
+        password2_error.innerHTML = "password does not match"
+    }
+    else{
+        password2_error.style.display = 'none'
+    }
+}
+
+const password1 = document.getElementById('password1')
+const password1_error = document.getElementById('password1_error')
+const password2 = document.getElementById('password2')
+const password2_error = document.getElementById('password2_error');
+const submit = document.getElementById('submit')
+
+password1.addEventListener('keyup', checkpassword1)
+
+password2.addEventListener('keyup', checkpassword2)
+
+submit.addEventListener('click',(evt)=>{
+    evt.preventDefault();
+    validateUname()
+    validateEmail()
+    checkpassword1()
+    checkpassword2()
+})
